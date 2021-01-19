768e3b629bc45375391121f1a61415731f7e0905
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2021-01-19 21:23:23 +0600

    Update Program.cs

--- Asymptotic/Program.cs
+++ Asymptotic/Program.cs
@@ -11,5 +11,25 @@ namespace Asymptotic
         static void Main(string[] args)
         {
         }
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; //O(1)
+            for (int i = 0; i < inputArray.Length; i++) //O(N)
+            {
+                for (int j = 0; j <inputArray.Length; j++) //O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) //O(N)
+                    {
+                        int y = 0; 
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+                        sum += inputArray[i] + i + k + j + y; 
+                    }
+                }
+            }
+            return sum; //O(1)
+        }//Итоговая O(2 + N3) (N в кубе)
     }
 }
